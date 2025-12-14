ccdfe0d3cabe7ded7e61ba78f1223fd457181781
maisha850 <tasnimmaisha08@gmail.com>
2025-12-14 22:18:39 +0600

    added live url at read me file

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 const instance = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://ticket-bari-server-eight.vercel.app'
 })
 const useAxios = () => {
     return instance
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 import useAuth from './useAuth';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://ticket-bari-server-eight.vercel.app'
     
 })
 
